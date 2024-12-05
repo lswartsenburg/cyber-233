002068ba6f9a8a97886e6504f4b39dc3b552fd45
Lucas Swartsenburg <lucas@dimmo.io>
2024-12-05 07:56:47 -0500

    Adding insta and organizing source tree

---
+++ apis/instagram.py
@@ -0,0 +1,27 @@
+import json
+import httpx
+
+client = httpx.Client(
+    headers={
+        # this is internal ID of an instegram backend app. It doesn't change often.
+        "x-ig-app-id": "936619743392459",
+        # use browser-like features
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "*/*",
+    }
+)
+
+
+def scrape_user(username: str):
+    """Scrape Instagram user's data"""
+    result = client.get(
+        f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}",
+    )
+    data = json.loads(result.content)
+    return data["data"]["user"]
+
+if __name__ == "__main__":
+    user_data = scrape_user("jeremyclarkson1")
+    print(json.dumps(user_data, indent=4))
